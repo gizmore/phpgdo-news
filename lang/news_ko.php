d43c44aec65b175d4daa077f6d8c5b0d088c756a
gizmore <gizmore@wechall.net>
2026-09-18 04:07:30 +0200

    'more korean'

---
+++ lang/news_ko.php
@@ -0,0 +1,72 @@
+<?php
+namespace GDO\News\lang;
+
+return [
+	'link_write_news' => '뉴스 쓰기',
+	'link_news' => '%s 뉴스%s',
+	'link_newsfeed' => 'RSS',
+	'link_newsletter' => '뉴스레터',
+	'link_newsletters' => '뉴스레터',
+	'newsfeed_title' => '뉴스피드',
+	'newsfeed_descr' => '%s의 일반 뉴스피드',
+	'news_comments' => '뉴스 댓글을 허용하시겠습니까?',
+	'news_guests' => '공개 뉴스 페이지?',
+	'newsletter_guests' => '공개 뉴스레터요?',
+	'news_guest_comments' => '손님 댓글을 허용하시겠습니까?',
+	'list_news_newslist' => '뉴스',
+	'newsletter_news' => '뉴스아이디',
+	'list_news_comments' => '뉴스 댓글',
+	'mt_news_write' => '뉴스 쓰기',
+	'div_texts' => '뉴스 텍스트',
+	'msg_news_created' => '뉴스 항목이 생성되었지만 여전히 표시되지 않습니다.',
+	'msg_news_edited' => '뉴스 항목이 수정되었습니다.',
+	'msg_news_visible' => '이제 뉴스 항목이 표시됩니다.',
+	'msg_news_queue' => '뉴스 항목이 메일 대기열로 이동되었습니다.',
+	'newsletter_status_waiting' => '뉴스 항목이 아직 메일 대기열에 추가되지 않았습니다.',
+	'newsletter_status_in_queue' => '뉴스 항목이 메일 대기열에 있습니다.',
+	'mt_news_writecomment' => '뉴스 댓글 쓰기',
+	'list_news_admin' => '뉴스(관리자)',
+	'mt_news_newsletterabbo' => '뉴스레터 구독',
+	'newsletter_subscribed' => '뉴스레터를 구독하고 싶습니다.',
+	'newsletter_sub_guest_unknown' => '게스트의 경우 뉴스레터 상태를 확인할 수 없습니다.',
+	'newsletter_info_subscribed' => '뉴스레터를 구독하셨습니다. 뉴스레터 옵션을 구성하려면 %s을(를) 확인하세요.',
+	'newsletter_info_not_subscribed' => '뉴스레터를 구독하지 않으셨습니다. 뉴스레터 옵션을 구성하려면 %s을(를) 확인하세요.',
+	'link_mail_settings' => '메일 설정',
+	'msg_newsletter_subscribed' => '뉴스레터를 구독하셨습니다.',
+	'msg_newsletter_unsubscribed' => '뉴스레터 구독을 취소하셨습니다.',
+	'err_newsletter_already_subscribed' => '이미 뉴스레터를 구독하셨습니다.',
+	'err_newsletter_not_subscribed' => '뉴스레터를 구독하지 않으셨습니다.',
+	'err_newsletter_no_email' => '뉴스레터를 구독하려면 이메일 주소를 제공해야 합니다.',
+	'dear_member_of' => '%s의 회원',
+	'mail_subj_newsletter' => '[%s] 뉴스 - %s',
+	'mail_body_newsletter' => '친애하는 %s님,
+
+뉴스레터 수신을 구독하신 %s에 뉴스 기사가 게시되었습니다.
+구독을 제거하려면 여기를 클릭하세요: %s
+
+아니면, 뉴스를 즐겨 읽어보세요!
+
+============================================================================================
+날짜: %s
+제목: %s
+작성자: %s
+============================================================================================
+%s
+============================================================================================
+
+친절한 감사
+%2$s 팀',
+	'cfg_news_blogbar' => '뉴스를 블로그 사이드바로 활성화하시겠습니까?',
+	'cfg_news_comments' => '뉴스에 댓글 엔진을 사용하시겠습니까?',
+	'cfg_news_guests' => '참석자가 뉴스를 읽도록 허용하시겠습니까?',
+	'cfg_newsletter_guests' => '손님을 위한 뉴스레터를 허용하시겠습니까?',
+	'cfg_news_guest_comments' => '참석자가 댓글을 달 수 있도록 허용하시겠습니까?',
+	'news_visible' => '보이는',
+	'news_sending' => '대기열에 있나요?',
+	'news_sent' => '보냄',
+	'list_news_newsletters' => '%s 뉴스레터가 폐기되었습니다.',
+	'rss_newsfeed' => '%s 뉴스피드',
+	'mt_news_rssfeed' => '뉴스피드',
+	'mt_news_unsubscribe' => '뉴스레터 구독 취소',
+	'news_read_mark' => '뉴스 리드마크',
+];
